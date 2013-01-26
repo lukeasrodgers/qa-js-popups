a4f2c66dd3a9b7b81dee827b5fc64503835dce72
Luke Rodgers <lukeasrodgers@gmail.com>
2013-01-26 13:32:13 -0500

    Ability to work with "constructor recipes". Also added slightly more informative error messages, better var names.

--- backbone.js
+++ backbone.js
@@ -1,15 +1,19 @@
 var queue = $({});
-jQuery.each(Application.Views, function(k, v) {
-  if (Application.Views.Window.prototype.isPrototypeOf(v.prototype)) {
+jQuery.each(Application.Views, function(class_name, Constructor) {
+  if (Application.Views.Window.prototype.isPrototypeOf(Constructor.prototype)) {
     queue.queue('windows', function(next) {
       try {
-        var win = new v();
+        var opts = (typeof constructor_recipes !== 'undefined') ? constructor_recipes[class_name] || {} : {};
+        if (typeof opts === 'function') {
+          opts = opts();
+        }
+        var win = new Constructor(opts);
         win.render();
         win.on('close', function() {
           next();
         });
       } catch (err) {
-        console.log('failed to open window', err);
+        console.log('failed to open window', class_name, err);
         next();
       }
     });
--- extjs.js
+++ extjs.js
@@ -1,9 +1,13 @@
 var queue = $({});
-jQuery.each(Application.Views, function(k, v) {
-  if (Ext.Window.prototype.isPrototypeOf(v.prototype)) {
+jQuery.each(Application.Views, function(class_name, Constructor) {
+  if (Ext.Window.prototype.isPrototypeOf(Constructor.prototype)) {
     queue.queue('windows', function(next) {
       try {
-        var win = new v();
+        var opts = (typeof constructor_recipes !== 'undefined') ? constructor_recipes[class_name] || {} : {};
+        if (typeof opts === 'function') {
+          opts = opts();
+        }
+        var win = new Constructor(opts);
         win.show();
         win.on('destroy', function() {
           next();
@@ -12,7 +16,7 @@ jQuery.each(Application.Views, function(k, v) {
           next();
         });
       } catch (err) {
-        console.log('failed to open window', err);
+        console.log('failed to open window', class_name, err);
         next();
       }
     });
