d66a4504cba0ea53c80d6bf732f6a37d27613919
Luke Rodgers <lukeasrodgers@gmail.com>
2013-01-26 15:27:29 -0500

    If window constructor is in blacklist, don't add to queue at all.

--- backbone.js
+++ backbone.js
@@ -1,12 +1,10 @@
 var queue = $({});
 jQuery.each(Application.Views, function(class_name, Constructor) {
-  if (Application.Views.Window.prototype.isPrototypeOf(Constructor.prototype)) {
+  if (typeof blacklist !== 'undefined' && blacklist.indexOf(class_name) !== -1) {
+    console.log('Skipping blacklisted', class_name);
+  }
+  else if (Application.Views.Window.prototype.isPrototypeOf(Constructor.prototype)) {
     queue.queue('windows', function(next) {
-      if (typeof blacklist !== 'undefined' && blacklist.indexOf(class_name) !== -1) {
-        console.log('Skipping blacklisted', class_name);
-        next();
-        return;
-      }
       try {
         console.log('Trying ', class_name);
         var opts = (typeof constructor_recipes !== 'undefined') ? constructor_recipes[class_name] || {} : {};
